ad2721c1820f2975fe28770567139831432bf67a
Camilo Nova <camilo.nova@axiacore.com>
2016-08-11 14:25:07 -0500

    Added site script

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # cachet-site-notifier
 A widget to be notified of status from a cachet website
+
+## Use
+Just add the cachet-site-notifier.js file to your html and:
+
+```
+document.addEventListener('DOMContentLoaded', function() {
+    loadCachet('https://my.cachet.site');
+}, false);
+```
+
+Brought to you by the fine folks @axiacore
+
---
+++ cachet-site-notifier.js
@@ -0,0 +1,40 @@
+function loadCachet(cachetUrl) {
+  var createAlert = function(incident) {
+    var divElement = document.createElement('div');
+    divElement.id = 'cachet-alert';
+    divElement.setAttribute('style', 'background-color: #FF3D2E; text-align: center; font-family: sans-serif;');
+    divElement.innerHTML = '<a style="color: #fff; display: block; padding: 5px;" href="'+ cachetUrl +'" target="_blank">'+ incident.name +'</a>';
+    document.body.insertBefore(divElement, document.body.children[0]);
+  };
+  
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+      if (xmlhttp.status == 200) {
+        var incident = JSON.parse(xmlhttp.responseText).data[0];
+        if (incident.status !== 4  && incident.status !== 0) {
+          createAlert(incident);
+        } else {
+          var xmlhttp2 = new XMLHttpRequest();
+          xmlhttp2.onreadystatechange = function() {
+            if (xmlhttp2.readyState == XMLHttpRequest.DONE) {
+              if (xmlhttp2.status == 200) {
+                var scheduledIncident = JSON.parse(xmlhttp2.responseText).data[0];
+                if (new Date(scheduledIncident.scheduled_at) > new Date()) {
+                  createAlert(scheduledIncident);
+                }
+              }
+            }
+          };
+          
+          xmlhttp2.open('GET', cachetUrl + '/api/v1/incidents/?sort=id&order=desc&per_page=1&status=0', true);
+          xmlhttp2.send();
+        }
+      }
+    }
+  };
+  
+  xmlhttp.open('GET', cachetUrl + '/api/v1/incidents/?sort=id&order=desc&per_page=1', true);
+  xmlhttp.send();
+}
+
