a4c128ca9f2ba9bddd1c26ff3cd4649c99e90c3f
Felipe Gonzalez <felipe.gonzalez@axiacore.com>
2016-08-12 11:57:26 -0500

    :bug: Fix error when status page not return incidents

--- cachet-site-notifier.js
+++ cachet-site-notifier.js
@@ -6,34 +6,38 @@ function loadCachet(cachetUrl) {
     divElement.innerHTML = '<a style="color: #fff; display: block; padding: 5px;" href="'+ cachetUrl +'" target="_blank">'+ incident.name +'</a>';
     document.body.insertBefore(divElement, document.body.children[0]);
   };
-  
+
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState == XMLHttpRequest.DONE) {
       if (xmlhttp.status == 200) {
         var incident = JSON.parse(xmlhttp.responseText).data[0];
-        if (incident.status !== 4  && incident.status !== 0) {
-          createAlert(incident);
-        } else {
-          var xmlhttp2 = new XMLHttpRequest();
-          xmlhttp2.onreadystatechange = function() {
-            if (xmlhttp2.readyState == XMLHttpRequest.DONE) {
-              if (xmlhttp2.status == 200) {
-                var scheduledIncident = JSON.parse(xmlhttp2.responseText).data[0];
-                if (new Date(scheduledIncident.scheduled_at) > new Date()) {
-                  createAlert(scheduledIncident);
+        if (incident !== null){
+          if (incident.status !== 4  && incident.status !== 0) {
+            createAlert(incident);
+          } else {
+            var xmlhttp2 = new XMLHttpRequest();
+            xmlhttp2.onreadystatechange = function() {
+              if (xmlhttp2.readyState == XMLHttpRequest.DONE) {
+                if (xmlhttp2.status == 200) {
+                  var scheduledIncident = JSON.parse(xmlhttp2.responseText).data[0];
+                  if (new Date(scheduledIncident.scheduled_at) > new Date()) {
+                    createAlert(scheduledIncident);
+                  }
                 }
               }
-            }
-          };
-          
-          xmlhttp2.open('GET', cachetUrl + '/api/v1/incidents/?sort=id&order=desc&per_page=1&status=0', true);
-          xmlhttp2.send();
+            };
+
+            xmlhttp2.open('GET', cachetUrl + '/api/v1/incidents/?sort=id&order=desc&per_page=1&status=0', true);
+            xmlhttp2.send();
+          }
+        } else {
+          console.log('cachet-site-notifier: No incidents reported');
         }
       }
     }
   };
-  
+
   xmlhttp.open('GET', cachetUrl + '/api/v1/incidents/?sort=id&order=desc&per_page=1', true);
   xmlhttp.send();
 }
